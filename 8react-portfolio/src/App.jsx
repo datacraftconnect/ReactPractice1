66691d23f50af345dea90afbda74580984f34795
datacraftconnect <datacraftconnect@gmail.com>
2025-08-23 18:05:48 +0100

    AddingAboutAndExperienceSection

--- 8react-portfolio/src/App.jsx
+++ 8react-portfolio/src/App.jsx
@@ -1,13 +1,20 @@
 // App.module.css - Styles for the main App component
 import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-import { Testbar } from './components/Testbar/testbar';
+import { Experience } from "./components/Experience/Experience";
 
 function App() {
 
 return (
   <div className={styles.app}>
     <Navbar />
+    <Hero />
+    <About />
+    <Experience />
+
+
 
 
   </div>
